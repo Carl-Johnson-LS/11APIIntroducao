87e34bb37c552fa0a3d3c81ae6de91113bef9638
Ronan Mendes Nogueira Couto <132668416+Carl-Johnson-LS@users.noreply.github.com>
2023-08-11 11:07:50 +0000

    T3

--- index.js
+++ index.js
@@ -64,4 +64,18 @@ app.delete("/usuario/:id", async (req, res) => {
   }
 });
 
-import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";
+
+app.patch("/usuario", async (req, res) => {
+  console.log("Rota PATCH /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.body.id);
+    if (usuario.length > 0) {
+      await updateUsuario(req.body);
+      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    console.log(error);
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
