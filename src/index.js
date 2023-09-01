c0267f30402232c6e4480672582ba1127780ddae
Ronan Mendes Nogueira Couto <132668416+Carl-Johnson-LS@users.noreply.github.com>
2023-09-01 11:31:35 +0000

    T2

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import dotenv from "dotenv";
 import express from "express";
 dotenv.config();
 
-import roteadorLogin from "./routes/login.js";
-import roteadorUsuario from "./routes/usuario.js";
-
-app.use(roteadorLogin);
-app.use(express.json());
-app.use(roteadorUsuario);
-
 const app = express();
 const port = 3000;
+
+import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
+
+app.use(express.json());
+app.use(roteadorUsuario);
+app.use(roteadorLogin);
 
 app.get("/", (req, res) => {
   res.json({
