e12da9f333989b099512fac3d111b7e496396344
Ronan Mendes Nogueira Couto <132668416+Carl-Johnson-LS@users.noreply.github.com>
2023-09-01 11:34:54 +0000

    T2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import dotenv from "dotenv";
 import express from "express";
+import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-import roteadorUsuario from "./routes/usuario.js";
-import roteadorLogin from "./routes/login.js";
-
 app.use(express.json());
 app.use(roteadorUsuario);
 app.use(roteadorLogin);
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json({
-    message: "API para CRUD usuario: https://github.com/Carl-Johnson-LS/11APIIntroducao.git",
+    message: "API para CRUD usuario: https://github.com/Alvesds-Ian/API-Introducao",
   });
 });
 
@@ -22,6 +22,3 @@ app.listen(port, () => {
   // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
-
-
-
